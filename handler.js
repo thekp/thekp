6be6fb02b157377c96943f677777d729602c45c4
thekp <khoa.phan@bbc.co.uk>
2020-07-24 18:52:47 +0100

    feat: add additional poke sprites

--- handler.js
+++ handler.js
@@ -8,22 +8,30 @@ const fetch = require('./lib/nodeFetch');
 
 const TEMPLATE_POKE_NAME = /{{ pokemon_name }}/g;
 const TEMPLATE_POKE_IMG = /{{ pokemon_img }}/g;
+const TEMPLATE_POKE_INFO = /{{ pokemon_info }}/g;
+
 const TEMPLATE_README =
-  "# Today's random Pokemon is... {{ pokemon_name }}\n\n![{{ pokemon_name }} shiny sprite]({{ pokemon_img }})";
+  "# Today's random Pokemon is... {{ pokemon_name }}\n\n![{{ pokemon_name }} shiny sprite]({{ pokemon_img }})\n\n<details>\n<summary>Additional about {{ pokemon_name }}</summary>\n{{ pokemon_info }} </details>";
+let initialTable = '\n| srpite type | image |\n|------|------|';
+
 const { GITHUB_TOKEN, GITHUB_USERNAME } = secrets;
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+});
 
 const updateReadme = async () => {
   const pokeAPI = `https://pokeapi.co/api/v2/pokemon/${getPokeNum()}`;
   const { sprites, name } = await fetch(pokeAPI);
 
-  const newReadMe = TEMPLATE_README.replace(
-    TEMPLATE_POKE_NAME,
-    upperFirst(name),
-  ).replace(TEMPLATE_POKE_IMG, sprites.front_shiny);
-
-  const octokit = new Octokit({
-    auth: GITHUB_TOKEN,
-  });
+  const entries = Object.entries(sprites);
+  for (const [view, link] of entries) {
+    if (link) {
+      initialTable += `\n| ${view} | ![{{ pokemon_name }} ${view} sprite](${link}) |`;
+    }
+  }
+  const newReadMe = TEMPLATE_README.replace(TEMPLATE_POKE_INFO, initialTable)
+    .replace(TEMPLATE_POKE_NAME, upperFirst(name))
+    .replace(TEMPLATE_POKE_IMG, sprites.front_shiny);
 
   const { data } = await octokit.request(
     `GET /repos/${GITHUB_USERNAME}/${GITHUB_USERNAME}/readme`,
